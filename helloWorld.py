e5632b2d1cbeb2c9cc1c02a7bf2fd5dda2e24bb1
KenbyCodes <154432189+KenbyCodes@users.noreply.github.com>
2024-03-13 18:54:18 +0000

    Add files via upload - ready for task cloning

---
+++ helloWorld.py
@@ -0,0 +1,77 @@
+# This short program will show how I can utilise git.
+
+
+
+
+# def hello_World():
+	# while True:
+
+
+
+
+
+user_Input = input(("\nPlease enter 'Hello World': "))		
+
+if user_Input == "Hello World!":
+	print("\n" + user_Input + "\n")
+
+
+
+
+# CODE ENDS HERE
+	
+
+
+
+
+
+
+'''
+if user_Input == 'Hello World!':
+				break
+			try:
+				user_Input = input(("\nPlease enter 'Hello World': "))
+			except ValueError:
+				if user_Input != 'Hello World!' and user_Input != '':
+					print("\n This is not the correct phrase - 'Hello World! ' - Please enter the correct phrase. ")
+				elif user_Input == int(user_Input) and user_Input != '':
+					print("\n Entry cannot be an interger or blank - Please enter the correct phrase. ")
+				elif user_Input == '':
+					print("\nError, entry cannot be blank. Please enter a value.")
+
+'''
+
+
+
+
+'''
+	def repeat():
+			Repeat = input("Would you like to run the program again? (Y / N) : ").lower()
+
+			if Repeat == 'y':
+				hello_World()  
+			if Repeat == 'n':
+				print("Thanks for using the Finance Calculator.")
+				exit()
+			if Repeat != 'y' or 'n' or '':
+				print("Invalid entry! Please enter a valid entry.")
+				repeat()					
+	repeat()
+	'''
+					
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
